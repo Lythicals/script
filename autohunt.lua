703ce942fd19dbd431bcfb3ef276803afac08c05
Lythicals <65028490+Lythicals@users.noreply.github.com>
2024-03-21 22:11:53 -0400

    Update autohunt.lua

--- autohunt.lua
+++ autohunt.lua
@@ -1160,7 +1160,7 @@ elseif ID == 16700725713 then
 elseif ID == 16537295657 then
     console()
     print("Game Detected: Hunt Hub")
-    if not hasBadge(4135484921332210) then gameTeleport(11329389795)
+    if not hasBadge(4135484921332210) then gameTeleport(11329389795) -- Astro Rennaisance
 
 
 
