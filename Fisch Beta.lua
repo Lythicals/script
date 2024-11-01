b0f124afe6595c7708efcf1d6dddb2b30ba12adb
Lythicals <65028490+Lythicals@users.noreply.github.com>
2024-11-01 19:05:53 -0400

    Create Fisch Beta.lua

---
+++ Fisch Beta.lua
@@ -0,0 +1,3 @@
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Lythicals/script/main/Fisch%20Beta.lua",true))()
+-- Join my new discord server @ discord.gg/lyth :) hope to see you there!
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lythicals/script/main/Fisch.lua",true))()
