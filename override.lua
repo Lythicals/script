ddd2b98c28fe7eba786f8e2ee9066005bed40394
Lythicals <65028490+Lythicals@users.noreply.github.com>
2024-12-20 10:32:43 -0500

    Update override.lua

--- override.lua
+++ override.lua
@@ -1,3 +1,4 @@
 return {
-  -- 4CD943A5-F747-4D32-96F3-B4CD7C687F18 <-- This is a unique ID, this is not representative of any personal information and is just used for identifying sessions remotely IF NEEDED!
+  -- 4CD943A5-F747-4D32-96F3-B4CD7C687F18, <-- This is a unique ID, this is not representative of any personal information and is just used for identifying sessions remotely IF NEEDED!
+  '2DC12EBD-C818-4EE7-8AB4-847D08871EFD',
 }
