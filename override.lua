42015f2a45eaf323df7076a6e8688aa4b33361a8
Lythicals <65028490+Lythicals@users.noreply.github.com>
2024-12-20 10:29:03 -0500

    Update override.lua

--- override.lua
+++ override.lua
@@ -1,3 +1,3 @@
 return {
-  ['d87gsd'] = 'manual override',
+  -- 4CD943A5-F747-4D32-96F3-B4CD7C687F18 <-- This is a unique ID, this is not representative of any personal information and is just used for identifying sessions remotely IF NEEDED
 }
